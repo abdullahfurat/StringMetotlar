d47168359e41f8487d5e8ccb125627913258c11a
muratvuranok <mrtvrnk@gmail.com>
2019-11-17 13:16:14 +0300

    Add project files.

---
+++ StringMetotlar/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace StringMetotlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTRIM = new System.Windows.Forms.Button();
+            this.btnTOCHARARRAY = new System.Windows.Forms.Button();
+            this.btnSUBSTRING = new System.Windows.Forms.Button();
+            this.btnTOLOWERTOUPPER = new System.Windows.Forms.Button();
+            this.btnINSERT = new System.Windows.Forms.Button();
+            this.btnSPLIT = new System.Windows.Forms.Button();
+            this.btnREPLACE = new System.Windows.Forms.Button();
+            this.btnREMOVE = new System.Windows.Forms.Button();
+            this.btnLASTINDEXOF = new System.Windows.Forms.Button();
+            this.btnINDEXOF = new System.Windows.Forms.Button();
+            this.btnENDSWITH = new System.Windows.Forms.Button();
+            this.btnSTARTSWITH = new System.Windows.Forms.Button();
+            this.btnCONTAINS = new System.Windows.Forms.Button();
+            this.txtGirisAlani = new System.Windows.Forms.TextBox();
+            this.btnCOMPARETO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTRIM
+            // 
+            this.btnTRIM.Location = new System.Drawing.Point(3, 571);
+            this.btnTRIM.Name = "btnTRIM";
+            this.btnTRIM.Size = new System.Drawing.Size(284, 35);
+            this.btnTRIM.TabIndex = 44;
+            this.btnTRIM.Text = "TRIM";
+            this.btnTRIM.UseVisualStyleBackColor = true;
+            // 
+            // btnTOCHARARRAY
+            // 
+            this.btnTOCHARARRAY.Location = new System.Drawing.Point(3, 530);
+            this.btnTOCHARARRAY.Name = "btnTOCHARARRAY";
+            this.btnTOCHARARRAY.Size = new System.Drawing.Size(284, 35);
+            this.btnTOCHARARRAY.TabIndex = 43;
+            this.btnTOCHARARRAY.Text = "TOCHARARRAY";
+            this.btnTOCHARARRAY.UseVisualStyleBackColor = true;
+            // 
+            // btnSUBSTRING
+            // 
+            this.btnSUBSTRING.Location = new System.Drawing.Point(3, 489);
+            this.btnSUBSTRING.Name = "btnSUBSTRING";
+            this.btnSUBSTRING.Size = new System.Drawing.Size(284, 35);
+            this.btnSUBSTRING.TabIndex = 42;
+            this.btnSUBSTRING.Text = "SUBSTRING";
+            this.btnSUBSTRING.UseVisualStyleBackColor = true;
+            // 
+            // btnTOLOWERTOUPPER
+            // 
+            this.btnTOLOWERTOUPPER.Location = new System.Drawing.Point(3, 448);
+            this.btnTOLOWERTOUPPER.Name = "btnTOLOWERTOUPPER";
+            this.btnTOLOWERTOUPPER.Size = new System.Drawing.Size(284, 35);
+            this.btnTOLOWERTOUPPER.TabIndex = 41;
+            this.btnTOLOWERTOUPPER.Text = "TOLOWER - TOUPPER";
+            this.btnTOLOWERTOUPPER.UseVisualStyleBackColor = true;
+            // 
+            // btnINSERT
+            // 
+            this.btnINSERT.Location = new System.Drawing.Point(3, 407);
+            this.btnINSERT.Name = "btnINSERT";
+            this.btnINSERT.Size = new System.Drawing.Size(284, 35);
+            this.btnINSERT.TabIndex = 40;
+            this.btnINSERT.Text = "INSERT";
+            this.btnINSERT.UseVisualStyleBackColor = true;
+            // 
+            // btnSPLIT
+            // 
+            this.btnSPLIT.Location = new System.Drawing.Point(3, 366);
+            this.btnSPLIT.Name = "btnSPLIT";
+            this.btnSPLIT.Size = new System.Drawing.Size(284, 35);
+            this.btnSPLIT.TabIndex = 39;
+            this.btnSPLIT.Text = "SPLIT";
+            this.btnSPLIT.UseVisualStyleBackColor = true;
+            // 
+            // btnREPLACE
+            // 
+            this.btnREPLACE.Location = new System.Drawing.Point(3, 325);
+            this.btnREPLACE.Name = "btnREPLACE";
+            this.btnREPLACE.Size = new System.Drawing.Size(284, 35);
+            this.btnREPLACE.TabIndex = 38;
+            this.btnREPLACE.Text = "REPLACE";
+            this.btnREPLACE.UseVisualStyleBackColor = true;
+            // 
+            // btnREMOVE
+            // 
+            this.btnREMOVE.Location = new System.Drawing.Point(3, 284);
+            this.btnREMOVE.Name = "btnREMOVE";
+            this.btnREMOVE.Size = new System.Drawing.Size(284, 35);
+            this.btnREMOVE.TabIndex = 37;
+            this.btnREMOVE.Text = "REMOVE";
+            this.btnREMOVE.UseVisualStyleBackColor = true;
+            // 
+            // btnLASTINDEXOF
+            // 
+            this.btnLASTINDEXOF.Location = new System.Drawing.Point(3, 243);
+            this.btnLASTINDEXOF.Name = "btnLASTINDEXOF";
+            this.btnLASTINDEXOF.Size = new System.Drawing.Size(284, 35);
+            this.btnLASTINDEXOF.TabIndex = 36;
+            this.btnLASTINDEXOF.Text = "LASTINDEXOF";
+            this.btnLASTINDEXOF.UseVisualStyleBackColor = true;
+            // 
+            // btnINDEXOF
+            // 
+            this.btnINDEXOF.Location = new System.Drawing.Point(3, 202);
+            this.btnINDEXOF.Name = "btnINDEXOF";
+            this.btnINDEXOF.Size = new System.Drawing.Size(284, 35);
+            this.btnINDEXOF.TabIndex = 35;
+            this.btnINDEXOF.Text = "INDEXOF";
+            this.btnINDEXOF.UseVisualStyleBackColor = true;
+            // 
+            // btnENDSWITH
+            // 
+            this.btnENDSWITH.Location = new System.Drawing.Point(3, 161);
+            this.btnENDSWITH.Name = "btnENDSWITH";
+            this.btnENDSWITH.Size = new System.Drawing.Size(284, 35);
+            this.btnENDSWITH.TabIndex = 34;
+            this.btnENDSWITH.Text = "ENDSWITH";
+            this.btnENDSWITH.UseVisualStyleBackColor = true;
+            // 
+            // btnSTARTSWITH
+            // 
+            this.btnSTARTSWITH.Location = new System.Drawing.Point(3, 120);
+            this.btnSTARTSWITH.Name = "btnSTARTSWITH";
+            this.btnSTARTSWITH.Size = new System.Drawing.Size(284, 35);
+            this.btnSTARTSWITH.TabIndex = 33;
+            this.btnSTARTSWITH.Text = "STARTSWITH";
+            this.btnSTARTSWITH.UseVisualStyleBackColor = true;
+            // 
+            // btnCONTAINS
+            // 
+            this.btnCONTAINS.Location = new System.Drawing.Point(3, 79);
+            this.btnCONTAINS.Name = "btnCONTAINS";
+            this.btnCONTAINS.Size = new System.Drawing.Size(284, 35);
+            this.btnCONTAINS.TabIndex = 32;
+            this.btnCONTAINS.Text = "CONTAINS";
+            this.btnCONTAINS.UseVisualStyleBackColor = true;
+            // 
+            // txtGirisAlani
+            // 
+            this.txtGirisAlani.Location = new System.Drawing.Point(3, 12);
+            this.txtGirisAlani.Name = "txtGirisAlani";
+            this.txtGirisAlani.Size = new System.Drawing.Size(284, 20);
+            this.txtGirisAlani.TabIndex = 31;
+            // 
+            // btnCOMPARETO
+            // 
+            this.btnCOMPARETO.Location = new System.Drawing.Point(3, 38);
+            this.btnCOMPARETO.Name = "btnCOMPARETO";
+            this.btnCOMPARETO.Size = new System.Drawing.Size(284, 35);
+            this.btnCOMPARETO.TabIndex = 30;
+            this.btnCOMPARETO.Text = "COMPARE TO";
+            this.btnCOMPARETO.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 617);
+            this.Controls.Add(this.btnTRIM);
+            this.Controls.Add(this.btnTOCHARARRAY);
+            this.Controls.Add(this.btnSUBSTRING);
+            this.Controls.Add(this.btnTOLOWERTOUPPER);
+            this.Controls.Add(this.btnINSERT);
+            this.Controls.Add(this.btnSPLIT);
+            this.Controls.Add(this.btnREPLACE);
+            this.Controls.Add(this.btnREMOVE);
+            this.Controls.Add(this.btnLASTINDEXOF);
+            this.Controls.Add(this.btnINDEXOF);
+            this.Controls.Add(this.btnENDSWITH);
+            this.Controls.Add(this.btnSTARTSWITH);
+            this.Controls.Add(this.btnCONTAINS);
+            this.Controls.Add(this.txtGirisAlani);
+            this.Controls.Add(this.btnCOMPARETO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTRIM;
+        private System.Windows.Forms.Button btnTOCHARARRAY;
+        private System.Windows.Forms.Button btnSUBSTRING;
+        private System.Windows.Forms.Button btnTOLOWERTOUPPER;
+        private System.Windows.Forms.Button btnINSERT;
+        private System.Windows.Forms.Button btnSPLIT;
+        private System.Windows.Forms.Button btnREPLACE;
+        private System.Windows.Forms.Button btnREMOVE;
+        private System.Windows.Forms.Button btnLASTINDEXOF;
+        private System.Windows.Forms.Button btnINDEXOF;
+        private System.Windows.Forms.Button btnENDSWITH;
+        private System.Windows.Forms.Button btnSTARTSWITH;
+        private System.Windows.Forms.Button btnCONTAINS;
+        private System.Windows.Forms.TextBox txtGirisAlani;
+        private System.Windows.Forms.Button btnCOMPARETO;
+    }
+}
+
